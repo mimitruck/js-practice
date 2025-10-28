4b82b210684a6997d1b9e29919f7f68805e01b07
mimitruck <heidiwang814@gmail.com>
2025-10-28 16:38:17 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //给button1绑定时间，先get element，然后add event listner
-const b1=document.getElementByid("button1");
+const b1=document.getElementById("button1");
 //得到的就是——整个 <button> 元素对象（DOM 元素
 b1.addEventListener( "click", function(){
   const a1="Dont click";
